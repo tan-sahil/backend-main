4c9b872f110b1f5d7a63d66e4bf1c4df01a045a8
tan-sahil <tha.sahu@gmail.com>
2023-12-26 00:24:53 +0530

    app  is configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import  dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
     path : './env'       // ./env root directory me ja kr env ko pkdo
@@ -8,4 +8,12 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()  // since itis a promise.
+.then(()=> {
+    app.listen(process.env.PORT|| 8000 , ()=> console.log(`app is listenig at port number ${process.env.PORT}`))
+    app.on('error', (error) => {
+        console.error("app is not loading : " , error);
+        throw error;
+    })
+}).catch((err) =>  console.log("error while reciving db", err));
+
