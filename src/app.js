cef3ef0733061e493432d36962906c056f51a46e
tan-sahil <tha.sahu@gmail.com>
2023-12-26 00:47:02 +0530

    configuration added to handle request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import express from "express";
-
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 const app = express();
+
+//using cors by setting its origin or white listing selcted url;
+
+app.use(cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true
+}))
+// for data provide as json body
+
+app.use(express.json({limit: "16kb"})) ; // here limit is useed so that unlimitd data is not fed to server
+
+// data can also be from url and encoding it is also important!!!!!!
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+
+// public file me file save krna
+
+app.use(express.static("public"));
+
+
 
 export {app};
