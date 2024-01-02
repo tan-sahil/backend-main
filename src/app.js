e11386932d7ba53af54c0fe375c379ef070bf305
tan-sahil <tha.sahu@gmail.com>
2024-01-03 01:58:15 +0530

    added controller with completion of setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
+
 const app = express();
 
 //using cors by setting its origin or white listing selcted url;
@@ -22,5 +23,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+
+/**routes import */
+import userRouter from "./routes/user.route.js";
+
+/**routes declaration */
+app.use("/api/v1/users", userRouter);
 
 export {app};
